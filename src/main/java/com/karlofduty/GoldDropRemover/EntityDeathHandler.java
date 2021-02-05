fc44293076b6d781d3d0e446b99b85831bd1c48f
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-05 19:47:33 +0100

    Clear drops from piglins & piglin brutes

--- src/main/java/com/karlofduty/GoldDropRemover/EntityDeathHandler.java
+++ src/main/java/com/karlofduty/GoldDropRemover/EntityDeathHandler.java
@@ -3,28 +3,37 @@ package com.karlofduty.GoldDropRemover;
 import org.bukkit.Material;
 import org.bukkit.entity.Drowned;
 import org.bukkit.entity.PigZombie;
+import org.bukkit.entity.Piglin;
+import org.bukkit.entity.PiglinBrute;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
-public class EntityDeathHandler implements Listener
-{
+public class EntityDeathHandler implements Listener {
     @EventHandler
-    public void onDeath(EntityDeathEvent event)
-    {
-        if(!(event.getEntity() instanceof PigZombie) && !(event.getEntity() instanceof Drowned))
-        {
-            return;
-        }
-
-        for(int i = 0; i < event.getDrops().size(); i++)
-        {
-            if(event.getDrops().get(i).getType().equals(Material.GOLD_INGOT) || event.getDrops().get(i).getType().equals(Material.GOLD_NUGGET) || event.getDrops().get(i).getType().equals(Material.GOLDEN_SWORD))
-            {
-                event.getDrops().remove(i);
-                i--;
-                GoldDropRemover.log("Removed gold drops from entity death");
+    public void onDeath(EntityDeathEvent event) {
+        if (event.getEntity() instanceof PigZombie || event.getEntity() instanceof Drowned ||
+            event.getEntity() instanceof Piglin || event.getEntity() instanceof PiglinBrute) {
+            for (int i = 0; i < event.getDrops().size(); i++) {
+                if (isGold(event.getDrops().get(i).getType()))
+                    event.getDrops().remove(i);
             }
+        }
+    }
+
+    private boolean isGold(Material material) {
+        switch(material) {
+            case GOLDEN_BOOTS:
+            case GOLDEN_LEGGINGS:
+            case GOLDEN_CHESTPLATE:
+            case GOLDEN_HELMET:
+            case GOLDEN_SWORD:
+            case GOLDEN_AXE:
+            case GOLD_INGOT:
+            case GOLD_NUGGET:
+                return true;
+            default:
+                return false;
         }
     }
 }
