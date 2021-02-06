e431d1c74f541edbcf7dccccd388db47ace6b728
Karl Essinger <xkaess22@gmail.com>
2021-02-06 16:15:15 +0100

    Formatting

--- src/main/java/com/karlofduty/GoldDropRemover/EntityDeathHandler.java
+++ src/main/java/com/karlofduty/GoldDropRemover/EntityDeathHandler.java
@@ -9,20 +9,26 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
-public class EntityDeathHandler implements Listener {
+public class EntityDeathHandler implements Listener
+{
     @EventHandler
-    public void onDeath(EntityDeathEvent event) {
+    public void onDeath(EntityDeathEvent event)
+    {
         if (event.getEntity() instanceof PigZombie || event.getEntity() instanceof Drowned ||
-            event.getEntity() instanceof Piglin || event.getEntity() instanceof PiglinBrute) {
-            for (int i = 0; i < event.getDrops().size(); i++) {
+            event.getEntity() instanceof Piglin || event.getEntity() instanceof PiglinBrute)
+        {
+            for (int i = 0; i < event.getDrops().size(); i++)
+            {
                 if (isGold(event.getDrops().get(i).getType()))
                     event.getDrops().remove(i);
             }
         }
     }
 
-    private boolean isGold(Material material) {
-        switch(material) {
+    private boolean isGold(Material material)
+    {
+        switch(material)
+        {
             case GOLDEN_BOOTS:
             case GOLDEN_LEGGINGS:
             case GOLDEN_CHESTPLATE:
