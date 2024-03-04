068852ace6ac53686ab5e270572cdfd45f91275d
Sculkin <158416970+Sculkinn@users.noreply.github.com>
2024-03-04 11:12:44 +1100

    Update EntityDeathHandler.java
    Added more gold items to list of blocked drops.

--- src/main/java/com/karlofduty/GoldDropRemover/EntityDeathHandler.java
+++ src/main/java/com/karlofduty/GoldDropRemover/EntityDeathHandler.java
@@ -32,14 +32,22 @@ public class EntityDeathHandler implements Listener
     {
         switch(material)
         {
-            case GOLDEN_BOOTS:
-            case GOLDEN_LEGGINGS:
-            case GOLDEN_CHESTPLATE:
-            case GOLDEN_HELMET:
-            case GOLDEN_SWORD:
-            case GOLDEN_AXE:
+            case GOLD_BLOCK:
             case GOLD_INGOT:
             case GOLD_NUGGET:
+            case GOLD_ORE:
+            case GOLDEN_APPLE:
+            case GOLDEN_AXE:
+            case GOLDEN_BOOTS:
+            case GOLDEN_CARROT:
+            case GOLDEN_CHESTPLATE:
+            case GOLDEN_HELMET:
+            case GOLDEN_HOE:
+            case GOLDEN_HORSE_ARMOR:
+            case GOLDEN_LEGGINGS:
+            case GOLDEN_PICKAXE:
+            case GOLDEN_SHOVEL:
+            case GOLDEN_SWORD:
                 return true;
             default:
                 return false;
